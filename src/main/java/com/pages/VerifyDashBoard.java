1569e667a57ec531e6de45125e7c8ccbc81e8cdd
Udhayakumar <udhayaarjvit1060@gmail.com>
2022-01-05 17:10:30 +0530

    third commit

--- src/main/java/com/pages/VerifyDashBoard.java
+++ src/main/java/com/pages/VerifyDashBoard.java
@@ -3,9 +3,13 @@ package com.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class VerifyDashBoard extends Login{
+public class VerifyDashBoard extends DashboardPage{
+    /**
+     * @description - here getting the Link count in dashboard page
+     */
     public void getLinkCount(){
         List<WebElement> listList = driver.findElements(By.xpath(properties.getProperty("Linkxpath")));
         int linkCount;
@@ -14,6 +18,11 @@ public class VerifyDashBoard extends Login{
         }
         System.out.println("Number of links "+linkCount);
     }
+
+    /**
+     * @description - click on AdminTab and check the checkBox is displayed and click it.
+     *
+     */
     public boolean isChecked(){
         driver.findElement(By.xpath(properties.getProperty("AdminTabXpath"))).click();
         WebElement checkBox = driver.findElement(By.xpath(properties.getProperty("CheckBoxXpath")));
@@ -23,6 +32,7 @@ public class VerifyDashBoard extends Login{
             System.out.println("Checkbox is selected");
         }
         return checkBox.isSelected();
+
     }
 
 }
--- src/main/java/com/seleniumBase/MyBrowser.java
+++ src/main/java/com/seleniumBase/MyBrowser.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.Duration;
 import java.util.Properties;
 
